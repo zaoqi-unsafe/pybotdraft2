22c531f4906dab5bf099274237bd283aa356acc5
zaoqi-unsafe <zaoqi-unsafe@users.noreply.github.com>
2019-02-03 10:41:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,12 @@
 class Bot(object):
     def __init__(self):
-        pass
+        self.group_message_callbacks=[]
+        self.private_message_callbacks=[]
+    def on_group_message(self):
+        def do_register(func): # func : Group, Message -> _
+            self.group_message_callbacks.append(func)
+        return do_register
+    def on_private_message(self):
+        def do_register(func): # func : Message -> _
+            self.private_message_callbacks.append(func)
+        return do_register
