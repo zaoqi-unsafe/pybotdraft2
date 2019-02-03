6fdaa323df59c5985fd8a906600cbf896d6967e6
zaoqi-unsafe <zaoqi-unsafe@users.noreply.github.com>
2019-02-03 11:03:40 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from typing import List, Callable
 class Group(object):
     def __init__(self):
         pass # WIP
@@ -9,17 +10,17 @@ class Message(object):
         pass # WIP
 class Bot(object):
     def __init__(self):
-        self.group_message_callbacks=[]
-        self.private_message_callbacks=[]
+        self.group_message_callbacks = []
+        self.private_message_callbacks = []
     def on_group_message(self):
-        def do_register(func): # func : Group, User, Message -> _
+        def do_register(func : Callable[[Group, User, Message], None]):
             self.group_message_callbacks.append(func)
         return do_register
     def _do_on_group_message(self, group : Group, sender : User, message : Message):
         for callback in self.group_message_callbacks:
             callback(group, sender, message)
     def on_private_message(self):
-        def do_register(func): # func : User, Message -> _
+        def do_register(func : Callable[[User, Message], None]):
             self.private_message_callbacks.append(func)
         return do_register
     def _do_on_private_message(self, sender : User, message : Message):
